283841222933c372358a4de52b8ff102b69180ed
Matsur Sviataslau <sviatdeveloper@gmail.com>
2024-03-03 20:15:31 +0300

    feat: add mutations

--- src/routes/graphql/graphqlSchemas/graphqlSchema.ts
+++ src/routes/graphql/graphqlSchemas/graphqlSchema.ts
@@ -1,6 +1,8 @@
 import { GraphQLSchema } from 'graphql/index.js';
 import RootQuery from './queries.js';
+import RootMutation from './mutations.js';
 
 export const graphqlSchema = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: RootMutation
 });
